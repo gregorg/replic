a5146b21deb5bb828c1504fd98a0a78ad32420d1
Gregory Duchatelet <greg@easyflirt.com>
2015-06-08 16:06:22 +0200

    [Add] setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: iso-8859-1 -*-
+# vim: ai ts=4 sts=4 et sw=4
+from setuptools import setup
+
+setup(
+    name="replic",
+    version="1.0",
+    license="MIT",
+    author="Grégory Duchatelet",
+    author_email="skygreg@gmail.com",
+    maintainer="Grégory Duchatelet",
+    maintainer_email="skygreg@gmail.com",
+    install_requires=["termcolor","mysql.connector"],
+    description="MariaDB replication checks and master switch",
+    url="https://github.com/gregorg/replic",
+    packages=[],
+    scripts=["replic.py"]
+)
