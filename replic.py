052ed0b88c6f83ff012506cd55b9409c5ae3fc9c
Gregory Duchatelet <greg@easyflirt.com>
2023-09-01 15:07:43 +0200

    poetry chdir

--- replic.py
+++ replic.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 # vim: ai ts=4 sts=4 et sw=4
-import os, sys
+import os, os.path, sys
 
 if __name__ == '__main__':
     binary = '/usr/bin/poetry'
     args = [binary, 'run', 'replic'] + sys.argv[1:]
+    os.chdir(os.path.abspath(os.path.basename(sys.argv[0]) + '/..'))
     os.execvp(binary, args)
 
 
